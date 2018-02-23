9e721262607a92dc204c3f44b7035b8bdfb8fd83
justi <justi@192.168.2.5>
2018-02-23 10:39:20 -0500

    added Juint test class outline

---
+++ src/Model/ProjectTest.java
@@ -0,0 +1,76 @@
+package Model;
+
+import org.junit.Test;
+
+public class ProjectTest {
+
+	// Defines a Board class contains 25 Location instances
+	@Test
+	public void LocationListTest() {
+
+	}
+
+	// Correctly reads codenames from a file named GameWords.txt and stores them in
+	// a List
+	@Test
+	public void ReadCodenameTest() {
+
+	}
+
+	// Creates List containing 25 distinct codenames selected at random
+	@Test
+	public void CodenameListTest() {
+
+	}
+
+	// Creates List containing randomly generated assignments for each of the person
+	// types
+	@Test
+	public void PersonListTest() {
+
+	}
+
+	// When game started, it is Red team's move and each of Board's 25 Location
+	// instances is assigned a codename, Person, and is Not Revealed
+	@Test
+	public void StartGameTest() {
+
+	}
+
+	// Method defined which correctly returns if a clue is legal or illegal
+	@Test
+	public void ClueLegalityTest() {
+
+	}
+
+	// Method defined which decrements the count, updates a Location when the
+	// Location's codename was selected, and returns if the Location contained the
+	// current team's Agent
+	@Test
+	public void CountTest() {
+
+	}
+	
+	@Test
+	public void LocationUpdateTest() {
+
+	}
+	
+	@Test
+	public void ContainsAgentTest() {
+
+	}
+	
+	// Method defined which correctly returns whether or not the Board is in one of the winning states
+	@Test
+	public void WinningStateTest() {
+
+	}
+	
+	// Method defined which correctly returns which team did not lose (i.e., win) when the Assassin was revealed
+	@Test
+	public void AssassinTest() {
+
+	}
+
+}
