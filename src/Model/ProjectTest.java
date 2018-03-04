ce7322c155735ca93b2dfa1ccbb381e957b945c8
aritrapaul <aritrapaul@10.84.123.46>
2018-03-04 18:53:52 -0500

    wrong assert fixed

--- src/Model/ProjectTest.java
+++ src/Model/ProjectTest.java
@@ -36,17 +36,14 @@ public class ProjectTest {
 		test.whosTurn(0);
 		assertTrue("Red is not the first team to start", test.getRedTurn());
 
-		assertfalse(test2.getCodeName() == null);
+		assertFalse(test2.getCodeName() == null);
 		assertTrue(test2.getCodeName() != "");
-		assertfalse(test2.getPerson() == null);
+		assertFalse(test2.getPerson() == null);
 		assertFalse(test2.isRevealed());
 
 	}
 
-	private void assertfalse(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 	/**
 	 * Correctly reads codenames from a file named GameWords.txt and stores them in
@@ -202,7 +199,7 @@ public class ProjectTest {
 
 	@Test
 	public void StartGameTest() {
-
+		
 	}
 
 	/**
